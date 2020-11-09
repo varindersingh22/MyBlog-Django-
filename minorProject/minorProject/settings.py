7040bd5dc4a1cb338caa0ebc1fa602adf8ea10a6
Shadow_supreme <67516832+Shadow-supreme22@users.noreply.github.com>
2020-11-09 15:13:06 +0530

    Update settings.py

--- minorProject/minorProject/settings.py
+++ minorProject/minorProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#_!bglia*el3#s&d-$lc271f+r@h=9_94$bb%0kqmdw!!l0ngk'
+SECRET_KEY = SECRET KEY HERE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
